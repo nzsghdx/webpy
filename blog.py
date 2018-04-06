d95cdc0ea63d1ca4e063692809035d5ee1a9b4a7
nzsghdx <dalao@nzsghdx.com>
2018-04-06 01:10:35 -0400

    my webpy blog

---
+++ blog.py
@@ -0,0 +1,83 @@
+# 导入所需包
+import web
+import model
+
+# 创建urls
+urls=(
+        # url对应四个视图页面(index.html,view.html,new.html,edit.html)?
+        '/','Index',
+        '/view/(\d+)','View',
+        '/new','New',
+        '/delete/(\d+)','Delete',
+        '/edit/(\d+)','Edit',
+)
+
+# 全局变量?
+t_globals={
+        
+        'datestr':web.datestr 
+        
+        }
+
+# 设置render
+render=web.template.render('templates',base='base',globals=t_globals)
+
+
+# 设置四个类,对应urls
+class Index:
+    def GET(self):
+        """Show page"""
+        # 在model中定义了get_posts函数,用于获取全部post
+        posts=model.get_posts()
+        return render.index(posts)
+
+class View:
+    def GET(self,id):
+        """View single post"""
+        # 根据id获取post
+        post = model.get_post(int(id))
+        return render.view(post)
+
+class New:
+    form=web.form.Form(
+            web.form.Textbox('title',web.form.notnull,size=30,description="Post title:"),
+            web.form.Textarea('content',web.form.notnull,rows=30,cols=80,description="Post content:"),
+            web.form.Button("Post entry"),
+            
+            )
+
+    def GET(self):
+         form=self.form()
+         return render.new(form)
+
+    def POST(self):
+         form=self.form()
+         if not form.validates():
+              return render.new(form)
+         model.new_post(form.d.title,form.d.content)
+         raise web.seeother('/')
+
+class Delete:
+    def POST(self,id):
+        model.del_post(int(id))
+        raise web.seeother('/')
+
+class Edit:
+    def GET(self,id):
+        post=model.get_post(int(id))
+        form=New.form()
+        form.fill(post)
+        return render.edit(post,form)
+
+    def POST(self,id):
+        form=New.form()
+        post=model.get_post(int(id))
+        if not form.validates():
+            return render.edit(post,form)
+        model.update_post(int(id),form.d.title,form.d.content)
+        raise web.seeother('/')
+
+app=web.application(urls,globals())
+
+if __name__=='__main__':
+    app.run()
